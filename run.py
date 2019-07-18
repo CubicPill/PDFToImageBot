a1f2ebbb620e0f3870e1d8677295678a02040b0e
CubicPill <CubicPill@users.noreply.github.com>
2019-07-18 18:55:04 +0800

    upload

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 import os
 
+import pdf2image
 from telegram import ReplyKeyboardRemove
 from telegram.ext import Updater, ConversationHandler, CommandHandler, MessageHandler, Filters
-import pdf2image
+
 TEMP_DIR = './temp'
 
-SEND_FILE, SELECT_SIZE, SELECT_PAGE = range(3)
+SEND_FILE, SELECT_SIZE, SELECT_PAGE, SELECT_FORMAT = range(4)
 
 
 def convert(bot, update):
+    update.message.reply_text('Send the PDF file to be converted')
+
+
+def cleanup(user_id):
     pass
 
 
@@ -16,6 +21,31 @@ def receive_file(bot, update):
     file = bot.get_file(update.message.document.file_id)
     file.download(os.path.join(TEMP_DIR, '{}.pdf'.format(update.message.from_user.id)))
     return SELECT_SIZE
+
+
+def select_dpi(bot, update):
+    return SELECT_PAGE
+
+
+def select_size(bot, update):
+    return SELECT_PAGE
+
+
+def select_page(bot, update):
+    return SELECT_FORMAT
+
+
+def select_format(bot, update):
+    do_convert(update.message.from_user.id)
+    cleanup(update.message.from_user.id)
+    return ConversationHandler.END
+
+
+def do_convert(user_id, size=None, dpi=None):
+    if not size and not dpi:
+        return False
+    pdf2image.convert_from_path(os.path.join(TEMP_DIR, '{}.pdf'.format(user_id)))
+    return True
 
 
 def cancel(update, context):
@@ -33,8 +63,8 @@ def main():
         entry_points=[CommandHandler('convert', convert)],
         states={
             SEND_FILE: MessageHandler(Filters.document.pdf, receive_file),
-            SELECT_SIZE: [],
-            SELECT_PAGE: []
+            SELECT_SIZE: [MessageHandler(Filters.text, select_dpi), MessageHandler(Filters.text, select_size)],
+            SELECT_PAGE: [MessageHandler(Filters.text, select_page)]
         },
         fallbacks=[CommandHandler('cancel', cancel)]
     ))
